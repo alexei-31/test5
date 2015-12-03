bcc181a28f6763cbfdeaf9429bcde05e7c2d6267
Alexey Garin <GarinAlexey@ya.ru>
2015-12-03 20:21:55 +0500

    add run_test.cmd

---
+++ run_test.cmd
@@ -0,0 +1,8 @@
+git init
+git commit -m "Initial commit"
+git push --set-upstream --force hg::https://bitbucket.org/Alexei_31/test5 master
+git add run_test.cmd
+git commit -m "add run_test.cmd"
+git push
+
+reset
